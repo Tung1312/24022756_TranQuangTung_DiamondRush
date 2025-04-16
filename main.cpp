6a9bdb7fabfab5de9b2a8c714ef614528fca1c9a
Tung1312 <offi20061312@gmail.com>
2025-04-16 11:35:49 +0700

    Khởi động

--- main.cpp
+++ main.cpp
@@ -1,111 +1,67 @@
-#include <iostream>
 #include "include/SDL.h"
 #include "include/SDL_image.h"
 #include "include/SDL_mixer.h"
-
-//Sửa lỗi "undefined reference to WinMain' collect2.exe: error: ld returned 1 exit status"
-#undef main
+#include "include/SDL_ttf.h"
+#include "SDL.h"
+#include <iostream>
 
 using namespace std;
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const char* WINDOW_TITLE = "Hello World!";
+#undef main
 
-void logErrorAndExit(const char* msg, const char* error)
-{
-    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
-    SDL_Quit();
-}
+int main(int argc, char* argv[]) {
+    //khởi động SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "Failed to initialize SDL: " << SDL_GetError() << endl;
+        return -1;
+    }
 
-SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE){
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-        logErrorAndExit("SDL_Init", SDL_GetError());
+    //cửa sổ
+    SDL_Window* window = SDL_CreateWindow(
+        "Diamond Rush",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        240, 320,
+        SDL_WINDOW_SHOWN
+    );
 
-    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    //full screen
-    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
+    if (!window) {
+        cout << "Failed to create window: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return -1;
+    }
 
-    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
-        logErrorAndExit( "SDL_image error:", IMG_GetError());
+    //render go brr brrr
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        cout << "Failed to create renderer: " << SDL_GetError() << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
-    return window;
-}
+    //main loop
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
 
-SDL_Renderer* createRenderer(SDL_Window* window){
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
-                                              SDL_RENDERER_PRESENTVSYNC);
-    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
-    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+        // Clear the screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
 
-    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
+        // Present the screen
+        SDL_RenderPresent(renderer);
+    }
 
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    return renderer;
-}
-
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer){
-    IMG_Quit();
-
+    //quét nhà
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-}
 
-void waitUntilKeyPressed(){
-    SDL_Event e;
-    while (true) {
-        if ( SDL_PollEvent(&e) != 0 &&
-             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
-            return;
-        SDL_Delay(100);
-    }
-}
-
-void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer){
-	SDL_Rect dest;
-
-	dest.x = x;
-	dest.y = y;
-	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
-
-	SDL_RenderCopy(renderer, texture, NULL, &dest);
-}
-
-SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer){
-	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
-
-	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
-	if (texture == NULL)
-        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
-
-	return texture;
-}
-
-int main(int argc, char *argv[]){
-    SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SDL_Renderer* renderer = createRenderer(window);
-
-    SDL_Texture* background = loadTexture("resource/bikiniBottom.jpg", renderer);
-    SDL_RenderCopy( renderer, background, NULL, NULL);
-
-    SDL_RenderPresent( renderer );
-    waitUntilKeyPressed();
-
-    SDL_Texture* spongeBob = loadTexture("resource/Spongebob.png", renderer);
-    renderTexture(spongeBob, 200, 200, renderer);
-
-    SDL_RenderPresent( renderer );
-    waitUntilKeyPressed();
-
-    SDL_DestroyTexture( spongeBob );
-    spongeBob = NULL;
-    SDL_DestroyTexture( background );
-    background = NULL;
-
-    quitSDL(window, renderer);
     return 0;
 }
